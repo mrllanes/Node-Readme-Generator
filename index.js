9af3f335f535846a4c95c88e37509208b6e8cefc
Mr. Llanes <llanesm@gmail.com>
2021-02-03 17:46:52 -0600

    Trying to do multiselect prompt. Not working yet.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const markdown = require('markdown-js');
+const multiPrompt = require('multiselect-prompt');
 const openFile = require('open');
 const questions = require('./questions');
 const makeReadme = require('./readme-template');
@@ -8,7 +10,7 @@ async function create() {
     try {
         const userInput = await inquirer.prompt(questions);
         const readme = makeReadme(userInput);
-        fs.writeFileSync('./Readme.md', md); // NOT SURE ABOUT THE "MD" HERE 
+        fs.writeFileSync('./Readme.md', readme);
         console.log('Thank you for using the Llanes Readme Maker 3000!');
         await openFile('./Readme.md');
     } catch (error) {
