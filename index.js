3200bd52be183114fca3e665a5bf1afbcf77ac25
Mr. Llanes <llanesm@gmail.com>
2021-02-06 09:58:21 -0600

    Added if/else for last question.

--- index.js
+++ index.js
@@ -9,20 +9,20 @@ async function create() {
     
     try {
         const userInput = await inquirer.prompt(questions);
-        if (data.confirm === true) {
-        const readme =  await makeReadme(userInput);
-        await fs.writeFileSync('./NewReadme/Readme.md', readme);
-        console.log('Thank you for using the Llanes Readme Maker 3000!');
-        console.log('The new Readme.md will now open in VIM for your immidiate pleasure.');
-        console.log('(in VIM, press the ESCAPE key and type ":q" to get out)');
-        console.log('Your New Professional Readme can be found in the "NewReadme" folder.');
-        await openFile('./NewReadme/Readme.md', {app:'vim'});
+        if (userInput.confirm === true) {
+            const readme =  await makeReadme(userInput);
+            await fs.writeFileSync('./NewReadme/Readme.md', readme);
+            console.log('Thank you for using the Llanes Readme Maker 3000!');
+            console.log('The new Readme.md will now open in "vim" for your immediate pleasure.');
+            console.log('(in vim, press the ESCAPE key and type ":q" to get out)');
+            console.log('Your New Professional Readme can be found in the "NewReadme" folder.');
+            await openFile('./NewReadme/Readme.md', {app:'vim'});
         } else {
             console.log('Program has been termintated. Please consider using the Llanes Readme Maker 3000 in the future!');
         }
     } catch (error) {
         console.log(error);
-    }
-}
+    };
+};
 
 create();
